1c6190281f542c1c79b6b6e7d81ee70b012006c8
Pranestya Gaung Wicaksono <117600120+Pranestya-GW@users.noreply.github.com>
2024-08-10 05:54:44 +0700

    Update generate_schema.sh

--- generate_schema.sh
+++ generate_schema.sh
@@ -6,46 +6,8 @@ JDBC_DRIVER="${GITHUB_WORKSPACE}/postgresql.jar"
 BASE_OUTPUT_DIR="${GITHUB_WORKSPACE}/generated"
 LOG_FILE="${BASE_OUTPUT_DIR}/schemaspy.log"  # Log file location
 
-# Define database connection parameters from environment variables
-DB_HOST="$DB_HOST"
-DB_PORT="$DB_PORT"
-DB_NAME="$DB_NAME"
-DB_USER="$DB_USER"
-DB_PASS="$DB_PASS"
-
 # Set the PGPASSWORD environment variable using the repository variable
 export PGPASSWORD="$DB_PASS"
-
-# Create log file if it doesn't exist
-if [ ! -f "$LOG_FILE" ]; then
-    touch "$LOG_FILE"
-fi
-
-# Test database connection
-echo "Testing database connection..."
-if ! psql -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" -U "$DB_USER" -c '\q'; then
-    echo "Failed to connect to the database. Please check your credentials and network connection." >> "$LOG_FILE"
-    exit 1
-fi
-
-echo "Database connection successful."
-
-# Ensure output directory exists
-mkdir -p "$BASE_OUTPUT_DIR"
-
-# Rotate old backups to keep only the last 7
-cd "$BASE_OUTPUT_DIR"
-
-# Check if any directories exist before rotating backups
-if [ "$(ls -d */ 2>/dev/null)" ]; then
-  # Rotate old backups if directories exist
-  ls -dt */ | tail -n +8 | xargs rm -rf
-else
-  # Create the first backup directory
-  echo "No directories found. Creating the first backup directory."
-  TIMESTAMP=$(date +"%Y-%m-%d-%H:%M")
-  mkdir -p "${BASE_OUTPUT_DIR}/${TIMESTAMP}"
-fi
 
 # Generate new output directory with timestamp
 TIMESTAMP=$(date +"%Y-%m-%d-%H:%M")
