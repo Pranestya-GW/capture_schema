631301b9fbf7ed66a949b6252d22033433bca0d1
Pranestya Gaung Wicaksono <117600120+Pranestya-GW@users.noreply.github.com>
2024-08-09 11:06:29 +0700

    Update generate_schema.sh

--- generate_schema.sh
+++ generate_schema.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-# Define variables
+# Define variables using environment variables passed by GitHub Actions
 SCHEMASPY_JAR="${GITHUB_WORKSPACE}/schemaspy-6.2.4.jar"
 JDBC_DRIVER="${GITHUB_WORKSPACE}/postgresql.jar"
 BASE_OUTPUT_DIR="${GITHUB_WORKSPACE}/generated"
 LOG_FILE="${GITHUB_WORKSPACE}/schemaspy.log"
 
-# Use environment variables for database credentials
 DB_HOST="${DB_HOST}"
 DB_PORT="${DB_PORT}"
 DB_NAME="${DB_NAME}"
 DB_USER="${DB_USER}"
-DB_PASSWORD="${DB_PASS}"
+DB_PASS="${DB_PASS}"
 
 # Ensure output directory exists
 mkdir -p $BASE_OUTPUT_DIR
 
 # Rotate old backups to keep only the last 7
 cd $BASE_OUTPUT_DIR
-ls -dt */ | tail -n +7 | xargs rm -rf
+
+# Check if any directories exist before rotating backups
+if [ "$(ls -d */ 2>/dev/null)" ]; then
+  # Rotate old backups if directories exist
+  ls -dt */ | tail -n +8 | xargs rm -rf
+else
+  # Create the first backup directory
+  echo "No directories found. Creating the first backup directory."
+  TIMESTAMP=$(date +"%Y-%m-%d-%H:%M")
+  mkdir -p "${BASE_OUTPUT_DIR}/${TIMESTAMP}"
+fi
 
 # Generate new output directory with timestamp
 TIMESTAMP=$(date +"%Y-%m-%d-%H:%M")
@@ -26,7 +35,7 @@ NEW_OUTPUT_DIR="${BASE_OUTPUT_DIR}/${TIMESTAMP}"
 mkdir -p $NEW_OUTPUT_DIR
 
 # Run SchemaSpy
-java -jar $SCHEMASPY_JAR -t pgsql -dp $JDBC_DRIVER -host $DB_HOST -port $DB_PORT -db $DB_NAME -u $DB_USER -p $DB_PASSWORD -s public -o $NEW_OUTPUT_DIR > $LOG_FILE 2>&1
+java -jar $SCHEMASPY_JAR -t pgsql -dp $JDBC_DRIVER -host $DB_HOST -port $DB_PORT -db $DB_NAME -u $DB_USER -p $DB_PASS -s public -o $NEW_OUTPUT_DIR > $LOG_FILE 2>&1
 
 # Check if SchemaSpy ran successfully
 if [ $? -ne 0 ]; then
